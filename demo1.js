980ff707e9996dc0f3bd0544d8c553404ae78074
jisa <jisa@live.com>
2022-02-06 10:35:33 +0530

    commit modified js files

---
+++ demo1.js
@@ -0,0 +1,22 @@
+switch(expression_here){
+ 
+case 1:
+ 
+// Execute JS code if case 1 evaluates as true
+ 
+break;
+ 
+case 2:
+ 
+// Execute JS code if case 2 evaluates as true
+ 
+break;
+ 
+default:
+ 
+// Execute JS code if none of the above cases are true
+ 
+break;
+ 
+}
+
